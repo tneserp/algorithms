afe908560090418bf7fc3a9f04901e29bb6aac50
한재현 <121341245+tneserp@users.noreply.github.com>
2024-04-13 16:44:55 +0900

    [Gold V] Title: 토마토, Time: 1992 ms, Memory: 48740 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,31 +3,36 @@ from collections import deque
 
 input = sys.stdin.readline
 
+M, N, H = map(int, input().split())
+
 dx = [-1, 0, 0, 1, 0, 0]
 dy = [0, -1, 0, 0, 1, 0]
 dz = [0, 0, -1, 0, 0, 1]
 
-M, N, H = map(int, input().split())
 board = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
-q = deque()
 
-for i in range(H):
-    for j in range(N):
-        for k in range(M):
-            if board[i][j][k] == 1:
-                q.append((i, j, k))
+def bfs():
+    q = deque()
+    for i in range(H):
+        for j in range(N):
+            for k in range(M):
+                if board[i][j][k] == 1:
+                    q.append((i, j, k))
 
-while q:
-    x, y, z = q.popleft()
-    for i in range(6):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        nz = z + dz[i]
-        if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M and board[nx][ny][nz] == 0:
-            board[nx][ny][nz] = board[x][y][z] + 1
-            q.append((nx, ny, nz))
+    while q:
+        x, y, z, = q.popleft()
 
+        for n in range(6):
+            nx = x + dx[n]
+            ny = y + dy[n]
+            nz = z + dz[n]
+            if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M and not board[nx][ny][nz]:
+                board[nx][ny][nz] = board[x][y][z] + 1
+                q.append((nx, ny, nz))
+
+
+bfs()
 ans = float("-inf")
 for i in range(H):
     for j in range(N):
