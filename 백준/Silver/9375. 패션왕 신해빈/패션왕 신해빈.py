010108f4f0708a3cc3a48cde315a3aa03a98ddaa
한재현 <121341245+tneserp@users.noreply.github.com>
2025-10-04 00:20:20 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,19 +1,24 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 T = int(input().strip())
 
-for i in range(T):
+for _ in range(T):
     n = int(input().strip())
-    fashion = dict()
-    for j in range(n):
+    ans = 1
+    item = {}
+    for _ in range(n):
         a, b = input().split()
-        if b not in fashion:
-            fashion[b] = 1
+
+        if b in item:
+            item[b].append(a)
         else:
-            fashion[b] += 1
-    sum = 1
-    for i in fashion.values():
-        sum *= i + 1
-    print(sum - 1)
+            item[b] = [a]
+
+    for i in item:
+        ans *= (len(item[i]) + 1)
+
+    ans -= 1
+    print(ans)
